a55c654ae2d74679437f84bd508ea33fd92be927
Christopher Richter <chrissy.richter2710@gmail.com>
2023-03-28 15:15:46 +0200

    Added basic funtionality to the UI like links

--- hightlighter-plugin/UI/js/script.js
+++ hightlighter-plugin/UI/js/script.js
@@ -1,4 +1,24 @@
+let toggleHighlight = document.getElementById("highlight")
+let toggleSummary = document.getElementById("summary")
+
+toggleHighlight.addEventListener("change", function() {
+    if (this.checked) {
+        console.log("CHECKED")
+    }
+    else {
+        console.log("UNCHECKED")
+    }
+})
+
+toggleSummary.addEventListener("change", function() {
+    if (this.checked) {
+        location.assign("./simba-summary.html")
+    }
+    else {
+        location.assign("./index.html")
+    }
+})
 
 
-    window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":550},"pages":[{"id":"page-simba-main","displayName":"Simba Main","link":{"linkType":"LinkTypePage","href":"#!page-simba-main"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-simba-summary","displayName":"Simba summary","link":{"linkType":"LinkTypePage","href":"#!page-simba-summary"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true},{"id":"page-simba-about","displayName":"Simba About","link":{"linkType":"LinkTypePage","href":"#!page-simba-about"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true},{"id":"page-simba-feedback-downvote","displayName":"Simba Feedback downvote","link":{"linkType":"LinkTypePage","href":"#!page-simba-feedback-downvote"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true},{"id":"page-simba-feedback-upvote","displayName":"Simba Feedback upvote","link":{"linkType":"LinkTypePage","href":"#!page-simba-feedback-upvote"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true}]}
+window.silex = window.silex || {}
+window.silex.data = {"site":{"width":550},"pages":[{"id":"page-simba-main","displayName":"Simba Main","link":{"linkType":"LinkTypePage","href":"#!page-simba-main"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-simba-summary","displayName":"Simba summary","link":{"linkType":"LinkTypePage","href":"#!page-simba-summary"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true},{"id":"page-simba-about","displayName":"Simba About","link":{"linkType":"LinkTypePage","href":"#!page-simba-about"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true},{"id":"page-simba-feedback-downvote","displayName":"Simba Feedback downvote","link":{"linkType":"LinkTypePage","href":"#!page-simba-feedback-downvote"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true},{"id":"page-simba-feedback-upvote","displayName":"Simba Feedback upvote","link":{"linkType":"LinkTypePage","href":"#!page-simba-feedback-upvote"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true}]}
