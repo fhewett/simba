78ada1aa5682b0502a77fdcb6564370602cbfbb1
Christopher Richter <chrissy.richter2710@gmail.com>
2023-05-05 10:22:22 +0200

    UI Changes final

--- hightlighter-plugin/UI/js/script.js
+++ hightlighter-plugin/UI/js/script.js
@@ -64,7 +64,4 @@ function respondToCall(message) {
     }
 }
 
-window.silex = window.silex || {}
-window.silex.data = { "site": { "width": 550 }, "pages": [{ "id": "page-simba-main", "displayName": "Simba Main", "link": { "linkType": "LinkTypePage", "href": "#!page-simba-main" }, "canDelete": true, "canProperties": true, "canMove": true, "canRename": true, "opened": false }, { "id": "page-simba-summary", "displayName": "Simba summary", "link": { "linkType": "LinkTypePage", "href": "#!page-simba-summary" }, "canDelete": true, "canRename": true, "canMove": true, "canProperties": true }, { "id": "page-simba-about", "displayName": "Simba About", "link": { "linkType": "LinkTypePage", "href": "#!page-simba-about" }, "canDelete": true, "canRename": true, "canMove": true, "canProperties": true }, { "id": "page-simba-feedback-downvote", "displayName": "Simba Feedback downvote", "link": { "linkType": "LinkTypePage", "href": "#!page-simba-feedback-downvote" }, "canDelete": true, "canRename": true, "canMove": true, "canProperties": true }, { "id": "page-simba-feedback-upvote", "displayName": "Simba Feedback upvote", "link": { "linkType": "LinkTypePage", "href": "#!page-simba-feedback-upvote" }, "canDelete": true, "canRename": true, "canMove": true, "canProperties": true }] }
-
 document.addEventListener('DOMContentLoaded', startSimba);
