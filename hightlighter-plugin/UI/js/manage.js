5a6d596e84915d068f41bbd1123223055d0b77e0
Christopher Richter <chrissy.richter2710@gmail.com>
2023-05-29 11:57:20 +0200

    Fixed #6

--- hightlighter-plugin/UI/js/manage.js
+++ hightlighter-plugin/UI/js/manage.js
@@ -2,16 +2,22 @@ let toggleHighlight = document.getElementById("highlight")
 let toggleSummary = document.getElementById("summary")
 
 toggleHighlight.addEventListener("change", saveOptions)
-    
-toggleHighlight.addEventListener("change", function () {
-    toggleVisibility(this, "highlightLoader")
-})
 
-toggleSummary.addEventListener("change", function () {
-    toggleVisibility(this, "summary")
-})
+toggleHighlight.addEventListener("change", function () { toggleVisibility(this, "highlightLoader") })
+
+toggleHighlight.addEventListener("change", function () { startOnToggleOn(this) })
+
+toggleSummary.addEventListener("change", function () { toggleVisibility(this, "summary") })
 
 toggleSummary.addEventListener("change", saveOptions)
+
+toggleSummary.addEventListener("change", function () { startOnToggleOn(this) })
+
+function startOnToggleOn(toggleButton) {
+    if (toggleButton.checked) {
+        startSimba()
+    }
+}
 
 function toggleVisibility(toggleButton, classname) {
     if (toggleButton.checked) {
@@ -40,6 +46,7 @@ function restoreOptions() {
         getSummary.then((res) => {
             toggleSummary.checked = res.summary
             toggleVisibility(toggleSummary, "summary")
+            toggleVisibility(toggleHighlight, "highlightLoader")
         });
     } catch (error) {
         console.log(error)
