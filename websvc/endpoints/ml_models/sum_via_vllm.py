571e76f06bf5f858f130f6287f07a56165de4cb7
Hadi Asghari <hadiasghari@users.noreply.github.com>
2024-02-13 14:19:53 +0100

    added preprocessing text and using model simba-v01c

--- websvc/endpoints/ml_models/sum_via_vllm.py
+++ websvc/endpoints/ml_models/sum_via_vllm.py
@@ -3,7 +3,7 @@ from difflib import SequenceMatcher as SM
 from openai import OpenAI
 from transformers import AutoTokenizer
 from .base_ml_model import BaseMLModel
-from ..utils import split_sentences
+from ..utils import split_sentences, keep_full_sentences
 
 
 
@@ -12,8 +12,8 @@ class SummaryViaVLLM(BaseMLModel):
     # 202402: this is again currently not used but kept in case 
 
     def __init__(self):
-        self.name = "VLLM-simba-v01b"
-        self.VLLM_MODEL = "hiig-piai/simba-v01b"
+        self.name = "VLLM-simba-v01c"
+        self.VLLM_MODEL = "hiig-piai/simba-v01c"
         self.VLLM_API_URL = "http://35.246.100.145:7001/v1"
         self.VLLM_API_KEY = os.environ.get('VLLM_API_KEY')
         self.MAX_LEN = 4096
@@ -23,33 +23,39 @@ class SummaryViaVLLM(BaseMLModel):
 
 
     def process(self, input_text):  
+        input_text = self.preprocess_text(input_text)
         input_text = self.truncate_text(input_text, self.MAX_LEN-self.SUMMARY_LEN-50)  # truncate to max token length for this API
 
         client = OpenAI(
             api_key = self.VLLM_API_KEY,
             base_url = self.VLLM_API_URL,
         )
-        prompt = "Du bist ein hilfreicher Assistent.\nUSER: Kannst du folgenden Zeitungsartikel vereinfachen: \n"
+        prompt = "Du bist ein hilfreicher Übersetzer für Leichte Sprache.\nUSER: Kannst du folgenden Text vereinfachen:\n"
         prompt += "```" + input_text + "```\nASSISTANT: "
 
         response = client.completions.create(model=self.VLLM_MODEL, prompt=prompt, max_tokens=120)  # set temperature=0.5
         output = response.choices[0].text  # this is just the model response (excludes prompt)
         # TODO: Re add this rule once we're sure re crash/empty outputs:  output = self.remove_repetitive_output(output)  
-        
         return output
 
 
+    def preprocess_text(self, raw_text): #whole_str is output from readability
+        sents = split_sentences(raw_text)
+        sents = keep_full_sentences(sents)
+        # note: possibly we might weant to also remove duplicates sentences 
+        cleaned_str = " ".join(sents)
+        return cleaned_str
+    
+
     def truncate_text(self, text, max_tokens):
         # Truncate the text if it exceeds the token limit
+        # note, if below is slow, one could do a rough word count with the formula `750 words ~= 1000 tokens`
         tokens = self.TKZ.encode(text, add_special_tokens=False)
         if len(tokens) > max_tokens:
             tokens = tokens[:max_tokens]
             text = self.TKZ.decode(tokens, skip_special_tokens=True)
         return text
         
-
-        return text
-
 
     def remove_repetitive_output(self, output):
         output_sents = split_sentences(output)
